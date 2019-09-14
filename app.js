604dfd3725a938fb2fb6d0e61ebf163774819521
Henry Lay <henry88lay@gmail.com>
2019-09-14 12:52:12 +1000

    created a folder called models and shifted the mongoose code to post.js

--- app.js
+++ app.js
@@ -1,18 +1,4 @@
 let express = require('express');
 let app = express();
-let mongoose = require('mongoose');
-let Schema = mongoose.Schema;
-
-let postSchema = new Schema({
-  id: Number,
-  title: String,
-  date: Date,
-  description: String,
-  text: String,
-  country: String,
-  imageURL: String
-});
-
-app.use(express.static('public'));
 
 app.listen(3000, () => console.log('Listening 3000....'));
