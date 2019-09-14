14bccd8106085174d3779c72789526eebd7fb611
Henry Lay <henry88lay@gmail.com>
2019-09-14 18:43:34 +1000

    created create-post.js file and declared vairiables

---
+++ public/admin/js/create-post.js
@@ -0,0 +1,9 @@
+let createForm = document.querySelector('#create-post-form');
+let createTitle = document.querySelector('#create-title');
+let createCountry = document.querySelector('#create-country');
+let createImageUrl = document.querySelector('#create-image-url');
+let createText = document.querySelector('#create-text');
+
+createForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+});
