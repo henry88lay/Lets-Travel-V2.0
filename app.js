1112e9fe2a16dc706dc525224bbc6d1abc88d402
Henry Lay <henry88lay@gmail.com>
2019-09-14 12:49:37 +1000

    coded and installed mongoose into the project

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 let express = require('express');
 let app = express();
+let mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+
+let postSchema = new Schema({
+  id: Number,
+  title: String,
+  date: Date,
+  description: String,
+  text: String,
+  country: String,
+  imageURL: String
+});
 
 app.use(express.static('public'));
 
