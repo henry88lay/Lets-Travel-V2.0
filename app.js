d9bc40fbc7605975a7d8f158cc072ad372adda32
Henry Lay <henry88lay@gmail.com>
2019-09-14 20:00:19 +1000

    successfully debugged the backend system to be able to load 2 posts

--- app.js
+++ app.js
@@ -4,10 +4,28 @@ let mongoose = require('mongoose');
 let Post = require('./models/posts').Post;
 
 mongoose.connect('mongodb://localhost/travels'), {useNewUrlParser: true};
+app.use(express.json());
+
+let id = 1;
 
 app.get('/posts', async (req, resp) => {
   let posts = await Post.find();
   resp.send(posts);
+});
+
+app.post('/posts', async (req, resp) => {
+  let reqBody = req.body;
+  let newPost = new Post({
+    id: id++,
+    title: reqBody.title,
+    date: new Date(),
+    description: reqBody.description,
+    text: reqBody.text,
+    country: reqBody.country,
+    imageURL: reqBody.imageUrl
+  });
+  await newPost.save();
+  resp.send('Created');
 });
 
 app.use(express.static('public'));
--- public/admin/js/create-post.js
+++ public/admin/js/create-post.js
@@ -1,4 +1,4 @@
-let createForm = document.querySelector('#create-post-form');
+let createForm = document.querySelector('.create-post-form');
 let createTitle = document.querySelector('#create-title');
 let createCountry = document.querySelector('#create-country');
 let createImageUrl = document.querySelector('#create-image-url');
@@ -6,4 +6,20 @@ let createText = document.querySelector('#create-text');
 
 createForm.addEventListener('submit', function(e) {
   e.preventDefault();
+  let text = createText.value;
+  fetch('http://localhost:3000/posts', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      title: createTitle.value,
+      country: createCountry.value,
+      imageUrl: createImageUrl.value,
+      text: text,
+      description: text.substring(0, text.indexOf('.') + 1)
+    })
+  })
+    .then(response => response.text())
+    .then(data => window.history.go());
 });
