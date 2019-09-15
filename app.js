8d17bf88c3776948944cc84587cd611f1e3bc46f
Henry Lay <henry88lay@gmail.com>
2019-09-15 20:21:40 +1000

    moved the route code base from app.js to route folder

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 let express = require('express');
 let app = express();
 let mongoose = require('mongoose');
-let Post = require('./models/posts').Post;
 let multer = require('multer');
-let uniqid = require('uniqid');
+let postsRouter = require('./routes/posts');
 
 mongoose.connect('mongodb://localhost/travels'), {useNewUrlParser: true};
 app.use(express.json());
@@ -13,36 +12,8 @@ let imageStorage = multer.diskStorage({
 });
 
 app.use(multer({storage: imageStorage}).single('imageFile'));
-
-app.get('/posts', async (req, resp) => {
-  let posts = await Post.find();
-  resp.send(posts);
-});
-
-app.post('/posts', async (req, resp) => {
-  let reqBody = req.body;
-  let imgPath;
-  if (reqBody.imageURL) {
-    imgPath = reqBody.imageURL;
-  } else {
-    imgPath = req.file.path.substring(
-      req.file.path.indexOf('/'),
-      req.file.path.length
-    );
-  }
-  let newPost = new Post({
-    id: uniqid(),
-    title: reqBody.title,
-    date: new Date(),
-    description: reqBody.description,
-    text: reqBody.text,
-    country: reqBody.country,
-    imageURL: imgPath
-  });
-  await newPost.save();
-  resp.send('Created');
-});
-
 app.use(express.static('public'));
+
+app.use('/posts', postsRouter);
 
 app.listen(3000, () => console.log('Listening 3000....'));
